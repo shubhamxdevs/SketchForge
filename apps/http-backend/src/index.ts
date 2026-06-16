4aabf955c72d13257cca4551e287bc7429a77ff7
Shubham Vishwakarma <sv8112004@gmail.com>
2026-06-16 09:05:59 +0530

    initializing a new common folder in packages: Backend-common

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,3 +1,42 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import {JWT_SECRET} from "@repo/backend-common/config";
+import {middleware} from "./middleware";
 const app = express();
-app.listen(3000);
+
+
+// Signup route 
+
+app.post("/signup",(req, res)=>{
+    //db Call
+    res.json({
+        userId:"123"
+    })
+
+});
+
+// Signin Route 
+
+app.post("/signin", (req, res)=>{
+
+    const userId = 1;
+    const token = jwt.sign({userId}, JWT_SECRET, {expiresIn: "24h"});
+
+    res.json({token});
+
+});
+
+// Room Route
+
+app.post("/room", middleware, (req, res)=>{
+    //db Call
+    res.json({
+
+        roomId:123
+    })
+
+
+});
+
+
+app.listen(3001);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,29 @@
 import {WebSocketServer} from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import {JWT_SECRET} from "@repo/backend-common/config";    
 
 const wss = new WebSocketServer({ port: 8080 });
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request) {
+    const url = request.url;
+    if(!url){
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') || "";
+    const decoded = jwt.verify(token, JWT_SECRET);
+   
+    if(typeof decoded == "string"){
+        ws.close();
+        return;
+
+    }
+   // If control reaches till here means decoded can't be a string and it must be JWTPayload
+   
+    if(!decoded || !decoded.userId){
+        ws.close();
+        return;
+    }
+
     ws.on('message', function message(data) {
         ws.send('pong');
     });
